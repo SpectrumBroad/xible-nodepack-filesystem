324317b92486d5ad0f95536d381f5da1b190bb9a
Jong <Stefan.de.Jong.wia@ing.com>
2018-10-20 16:29:00 +0200

    support multiple directories
    By means of a paths input.

--- filesystem.directory/index.js
+++ filesystem.directory/index.js
@@ -1,9 +1,12 @@
 'use strict';
 
 module.exports = (NODE) => {
-  const directoryOut = NODE.getOutputByName('directory');
+  const pathsIn = NODE.getInputByName('paths');
 
-  directoryOut.on('trigger', async (conn, state) => {
-    return NODE.data.path;
+  const directoriesOut = NODE.getOutputByName('directories');
+  directoriesOut.on('trigger', async (conn, state) => {
+    const paths = await pathsIn.getValues(state);
+
+    return paths.length ? paths : NODE.data.path;
   });
 };
